6b4f2b5ecebd808beafba1b4a2431e6c7c1415cb
alfie <40274769+diz-zzyy@users.noreply.github.com>
2022-11-25 13:59:05 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -13,22 +13,23 @@ if game.PlaceId ~= 2788229376 then
     messagebox("You are not in the correct game, please ensure you are in 'Da Hood'.", "Agony", 0)
     error("You are not in the correct game, please ensure you are in 'Da Hood'.")
     return
+else
+    print("DaHood Detected!")
 end
 
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
-repeat wait() until game.Players.LocalPlayer.Character ~= nil
 repeat wait() until game:IsLoaded()
-wait(3)
+wait(1)
 
-
-print("Loading Commands...")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diz-zzyy/AgonyDropper/main/Commands.lua"))()
 
 print("Commands Loaded!")
+print("Loading AntiAFK...")
 
 local VirtualUser = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
+    print("AntiAFK running...")
     VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
     wait(1)
     VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
@@ -37,22 +38,25 @@ end)
 print("AntiAFK ready.")
 print("Loading Command Handler...")
 
-function Command(player, msg)
-      msg = string.lower(msg)
-      cmd = string.split(msg," ")
-      print("Operator chatted: " .. cmd[1])
-      if(string.sub(cmd[1],1,1) == config.Prefix) then
-           cmd1 = cmd[1]:gsub(config.Prefix, "")
-          if(_G.AgonyCommands[cmd1] ~= nil) then
-              print("Running Command " .. cmd1 + "!")
-              _G.AgonyCommands[cmd1]({cmd[2], cmd[3], cmd[4], cmd[5], cmd[6], cmd[7], cmd[8], cmd[9], cmd[10], cmd[11]}, player)
-          else
-            print("No comand for " .. cmd1 + "!")
-          end
-      end
+function Command(player)
+    player.Chatted:Connect(function(msg)
+        msg = string.lower(msg)
+        cmd = string.split(msg," ")
+        print("Operator chatted: " .. cmd[1])
+        if(string.sub(cmd[1],1,1) == config.Prefix) then
+             cmd1 = cmd[1]:gsub(config.Prefix, "")
+            if(_G.AgonyCommands[cmd1] ~= nil) then
+                print("Running Command " .. cmd1 + "...")
+                _G.AgonyCommands[cmd1]({cmd[2], cmd[3], cmd[4], cmd[5], cmd[6], cmd[7], cmd[8], cmd[9], cmd[10], cmd[11]}, player)
+            end
+        end
+    end)
 end
 
---[[for _,player in pairs(game.Players:GetPlayers()) do
+print("Command Handler ready.")
+print("Loading Players...")
+
+for _,player in pairs(game.Players:GetPlayers()) do
     print(player.Name .. " being checked.")
     for _,v in pairs(config.Controllers) do
        if(tostring(player.UserId) == tostring(v)) then
@@ -70,34 +74,12 @@ game.Players.PlayerAdded:Connect(function(player)
             Command(player)
         end
     end
-end)]]--
-
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local LocalPlayer = Players.LocalPlayer
-
-ReplicatedStorage.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(Data)
-    -- // Get all of the data
-    local player = Players[Data.FromSpeaker]
-    local msg = Data.Message
-    local channel = Data.OriginalChannel
-
-    -- // Ignore if it is LocalPlayer as that event already fires
-    if (Player == LocalPlayer) then
-        return
-    end
-        
-    for _,v in pairs(config.Controllers) do
-        if(tostring(player.UserId) == tostring(v)) then
-            Command(player, msg)
-        end
-    end
 end)
 
-print("Command Handler ready.")
+print("Players ready.")
 print("Finishing off...")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PickleIsDaBest/Min/main/Skript"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Other/ChattedFix.lua"))()
 
 print("Agony loaded!")
