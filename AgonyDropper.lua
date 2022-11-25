99d7009fc84846c90104c5d645f492534fc5bd6a
alfie <40274769+diz-zzyy@users.noreply.github.com>
2022-11-25 22:34:19 +0000

    Update AgonyDropper.lua

--- AgonyDropper.lua
+++ AgonyDropper.lua
@@ -1,9 +1,14 @@
 _G.AgonyDropper = {
+    -- Controllers : Enter the UserID's of the players that can control the bots.
     Controllers = {
         "12813448",
     },
     
+    -- Prefix : Enter the prefix before each command.
     Prefix = "!",
+    
+    -- Max FPS : Enter the Max FPS for the alts.
+    MaxFps = 3,
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/diz-zzyy/AgonyDropper/main/Loader.lua"))()
