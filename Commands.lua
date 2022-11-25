e3457676ea18dcf0a181c741f2965b8a6f935bc4
alfie <40274769+diz-zzyy@users.noreply.github.com>
2022-11-25 12:22:14 +0000

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -24,4 +24,25 @@ cmds["chat"] = function(args)
   end
 end
 
+cmds["tp"] = function(args)
+  if(args[1] == "" or args[1] == nil) then
+     game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Please input CFrame to teleport to.","All")
+  else
+       local split_cframe = string.split(args[1],",")
+       local x,y,z = split_cframe[1],split_cframe[2],split_cframe[3]
+       game.Players:GetPlayerByUserId(v).Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
+  end
+end
+
+cmds["tpf"] = function(args)
+    if(args[1] == "" or args[1] == nil) then
+     game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Please input CFrame to teleport to.","All")
+  else
+       local split_cframe = string.split(args[1],",")
+       local x,y,z = split_cframe[1],split_cframe[2],split_cframe[3]
+       player.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
+       player.Character.HumanoidRootPart.Anchored = true
+  end
+end
+
 _G.AgonyCommands = cmds
